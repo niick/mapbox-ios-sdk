b8c9e55cc02b9f5fd50ada7acda7c9e1d5d37109
Justin R. Miller <incanus@codesorcery.net>
2012-02-21 17:13:00 -0800

    refs #23: use NSURLConnection for network tile fetches

--- MapView/Map/RMAbstractWebMapSource.h
+++ MapView/Map/RMAbstractWebMapSource.h
@@ -29,7 +29,7 @@
 #import "RMProjection.h"
 
 #define RMAbstractWebMapSourceRetryCount  3
-#define RMAbstractWebMapSourceWaitSeconds 2
+#define RMAbstractWebMapSourceWaitSeconds 2.0f
 
 @interface RMAbstractWebMapSource : RMAbstractMercatorTileSource
 
